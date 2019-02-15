26d0797786fc30e0cf1da17a8ed651e3df22d6c0
libing <18310275531@163.com>
2019-02-15 14:01:13 +0800

    '2a'

--- users/urls.py
+++ users/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
+from django.conf.urls import url
 from django.contrib import admin
+from users import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^user/',include('users.urls',namespace='user')),
+    url(r'^register/$', views.register,name='register')
 ]
